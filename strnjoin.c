b989ba8e4909a2ee2aeb81df9fac5542c408577e
timon <timonprata@gmail.com>
2016-01-02 02:11:33 +0100

    dont fortet

--- strnjoin.c
+++ strnjoin.c
@@ -22,7 +22,7 @@ char	*ft_strnjoin(char *s1, char *s2, int n)
   int	k;
 
   k = 0;
-  j = ft_strlen(s1) + ft_strlen(s2) - n;
+  j = ft_strlen(s1) + ft_strlen(s2) - (ft_strlen(s2) - n);
   if (!(str = (char *)malloc(sizeof(char) * (j + 1))))
     return (NULL);
   i = -1;
