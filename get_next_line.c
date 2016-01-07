20e234e2a7ffd65ec2d39b9470757f0548fd9853
Timon PRATA <tiprata@e3r11p21.42.fr>
2016-01-07 18:54:22 +0100

    kokok

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/14 16:30:50 by tiprata           #+#    #+#             */
-/*   Updated: 2015/12/30 19:39:58 by tiprata          ###   ########.fr       */
+/*   Updated: 2016/01/07 18:13:27 by tiprata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,59 +21,76 @@ int	get_next_line(int const fd, char **line)
   int i;
   int j;
   int count;
-  
+  int endof;
+
+  endof = 0;
   count = 0;
   j = 0;
   i = 0;
-  stop = 0; // IF DEUX BACKSLASH N DAFFILE ATTENTION LA FONCTION BEUGUE
+  stop = 0;
   if (s.str != NULL)
-    {
+  {
+	  ft_bluestr(s.str);
       stop = ft_strchr(s.str, '\n');
       if (stop > 0)
-		{
-	  *line = ft_strsub(s.str, 0, stop - 1);
-	  while (i != stop + 1)
-	    {
-	      s.str++;
-	      i++;
-	    }
-	  if (*s.str == '\0')
-	    {
-				free(s.tmp);
-				s.str = NULL;
-				s.tmp = NULL;
-		}
-		return (s.ret);
-		}
-		stop = 0;
-		stock = ft_strdup(s.str);
-	}
-	else
-		stock = ft_memalloc(BUF_SIZE + 1);
-	while ((s.ret = read(fd, tmp, BUF_SIZE)))
-    {
-		tmp[s.ret] = '\0';
-		stop = ft_strchr(tmp, '\n');
-		if (stop >= 0 && s.str == NULL)
-		{
-			//	s.str = ft_strnew(ft_strlen(tmp) - stop - 1);
-			stock = ft_dupnstrcat(stock, tmp, stop - 1);
-			s.str = ft_strsub(tmp, stop + 1, ft_strlen(tmp) - stop - 1);
-			s.tmp = s.str;
-			*line = ft_strdup(stock);
-			return (s.ret);
-		}
-		else
-		{
-			stock = ft_dupstrcat(stock, tmp);
-			free(s.str);
-			s.str = NULL;
-		}
-    }
-	*line = ft_strdup(stock);
-	free(stock);
-	stock = NULL;
-	return (s.ret);
+	  {
+		  ft_putstr("_____");
+		  *line = ft_strsub(s.str, 0, stop - 1);
+		  while (i != stop + 1)
+		  {
+			  s.str++;
+			  i++;
+		  }
+		  if (*s.str == '\0')
+		  {
+			  free(s.tmp);
+			  s.str = NULL;
+			  s.tmp = NULL;
+		  }
+		  return (s.ret);
+	  }
+	  stop = 0;
+	  stock = ft_strdup(s.str);
+	  if (s.ret == 0)
+	  {
+		  ft_pinkchar('X');
+		  *line = ft_strdup(stock);
+		  return (s.ret);
+	  }
+	  ft_pinkchar('[');
+	  ft_greenstr(stock);
+	  ft_pinkchar(']');
+  }
+  else
+	  stock = ft_memalloc(BUF_SIZE + 1);
+  while ((s.ret = read(fd, tmp, BUF_SIZE)))
+  {
+	  tmp[s.ret] = '\0';
+	  stop = ft_strchr(tmp, '\n');
+	  if (stop >= 0 && s.str == NULL)
+	  {
+		  stock = ft_dupnstrcat(stock, tmp, stop - 1);
+		  s.str = ft_strsub(tmp, stop + 1, ft_strlen(tmp) - stop + 1);
+		  s.tmp = s.str;
+		  *line = ft_strdup(stock);
+		  ft_redstr(*line);
+		  ft_bluestr("||");
+		  return (s.ret);
+	  }
+	  else// if (stop < 0)
+	  {
+		  stock = ft_dupstrcat(stock, tmp);
+		  ft_putstr("-->");
+		  ft_bluestr(stock);
+		  ft_putstr("<--");
+		  free(s.str);
+		  s.str = NULL;
+	  }
+  }
+  *line = ft_strdup(stock);
+  free(stock);
+  stock = NULL;
+  return (s.ret);
 }
 
 int	main(int ac, char **av)
@@ -89,18 +106,22 @@ int	main(int ac, char **av)
 	if (ac > 1)
 	{
 		fd = open(av[1], O_RDONLY);
-		while ((ret = get_next_line(fd, &line[i])))
+		while ((ret = get_next_line(fd, &line[i])) || ret == 0)
 		{
+			ft_putchar('\n');
+			ft_putendl(line[i]);
 			i++;
+			if (ret == 0)
+				break ;
 		}
 	}
 	line[i] = NULL;
-	i = 0;
+/*	i = 0;
 	while (line[i])
 	{
 		ft_putstr(line[i]);
 		ft_putchar('\n');
 		i++;
-	}
+		}*/
 	return (0);
 }
