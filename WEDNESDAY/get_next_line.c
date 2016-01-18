fe5ac474f7738a916c4aa38ebf0e55cf20e38320
Timon PRATA <tiprata@e3r7p23.42.fr>
2016-01-18 17:23:10 +0100

    okay

--- WEDNESDAY/get_next_line.c
+++ WEDNESDAY/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tiprata <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/10 19:40:34 by tiprata           #+#    #+#             */
-/*   Updated: 2016/01/18 16:54:39 by tiprata          ###   ########.fr       */
+/*   Updated: 2016/01/18 17:22:30 by tiprata          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,6 +68,7 @@ int		ft_static_exist(char **s, char **line, char **str, int ret)
 	{
 //		ft_redstr("bah nan");
 		*line = ft_strdup(*s);
+		ft_strdel(s);
 		return (2);
 	}
 	else
@@ -78,6 +79,7 @@ int		ft_static_exist(char **s, char **line, char **str, int ret)
 		{
 			*str = *str + 1;
 			*s = ft_strdup(*str);
+//			ft_strdel(str);
 		}
 		else
 			*s = NULL;
@@ -85,37 +87,10 @@ int		ft_static_exist(char **s, char **line, char **str, int ret)
 	}
 }
 
-/*int		ft_read(t_rest *st, char **str, char **line, int const fd)
-{
-	while (st->j == 0)
-	{
-		st.tmp = st.tmp == NULL ? ft_memalloc(BUF_SIZE + 1) : st.tmp;
-		if ((st->ret = read(fd, st->tmp, BUF_SIZE)) && st->ret == -1)
-			return (-1);
-		st->j = st->ret == 0 ? 1 : 0;
-		st->tmp[st->ret + 1] = '\0';
-		*str = ft_strchr(st->tmp, '\n');
-		if (str == NULL)
-		{
-		*line = ft_dupstrcat(*line, st->tmp, ft_strlen(st->tmp));
-		ft_strdel(&st->tmp);
-		}
-		else //if (str != NULL && (st->i = *str - st->tmp) && st->i >= 0) 
-		{
-//			st->i = *str - st->tmp;
-			*line = ft_dupstrcat(*line, st->tmp, st->i);
-			st->s = ft_strdup(str[1]);
-			st->sfree = st->s;
-			ft_strdel(&st->tmp);
-			return (st->ret == 0 ? ft_strd(&st->s, str) : 1);
-		}
-	}
-	}*/
 int		get_next_line(int const fd, char **line)
 {
 	static t_rest	st;
 	char			*str;
-	int top;
 
 	str = NULL;
 	st.i = 0;
@@ -126,7 +101,7 @@ int		get_next_line(int const fd, char **line)
 		return (-1);
 	*line = NULL;
 	if (st.s)
-		if ((top = ft_static_exist(&st.s, line, &str, st.ret)) != 2)
+		if ((ft_static_exist(&st.s, line, &str, st.ret)) != 2)
 			return (st.ret == 0 && st.s[0] == '\0' ? ft_strd(&st.s, &str) : 1);
 //	ft_read(&st, &str, line, fd);
 	while (st.j == 0)
@@ -148,7 +123,6 @@ int		get_next_line(int const fd, char **line)
 			*line = ft_dupstrcat(*line, st.tmp, st.i);
 			if (st.s)
 			{
-//				ft_redstr(st.s);
 				ft_strdel(&st.sfree);
 				st.s = NULL;
 			}
@@ -185,8 +159,8 @@ int   main(int ac, char **av)
 	ret = 0;
 	if (ac > 1)
 	{
-		while (1)
-		{
+//		while (1)
+//		{
 			fd = open(av[1], O_RDONLY);
 			if (fd == -1)
 		{
@@ -201,7 +175,34 @@ int   main(int ac, char **av)
 				ft_strdel(&line);
 			}
 			close(fd);
-		}
+			//	}
 	}
 	return (0);
 	}
+
+ /*int		ft_read(t_rest *st, char **str, char **line, int const fd)
+{
+	while (st->j == 0)
+	{
+		st.tmp = st.tmp == NULL ? ft_memalloc(BUF_SIZE + 1) : st.tmp;
+		if ((st->ret = read(fd, st->tmp, BUF_SIZE)) && st->ret == -1)
+			return (-1);
+		st->j = st->ret == 0 ? 1 : 0;
+		st->tmp[st->ret + 1] = '\0';
+		*str = ft_strchr(st->tmp, '\n');
+		if (str == NULL)
+		{
+		*line = ft_dupstrcat(*line, st->tmp, ft_strlen(st->tmp));
+		ft_strdel(&st->tmp);
+		}
+		else //if (str != NULL && (st->i = *str - st->tmp) && st->i >= 0) 
+		{
+//			st->i = *str - st->tmp;
+			*line = ft_dupstrcat(*line, st->tmp, st->i);
+			st->s = ft_strdup(str[1]);
+			st->sfree = st->s;
+			ft_strdel(&st->tmp);
+			return (st->ret == 0 ? ft_strd(&st->s, str) : 1);
+		}
+	}
+	}*/
